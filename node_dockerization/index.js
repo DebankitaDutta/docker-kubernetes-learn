e51be66cc6f57d8986281a17040cf0410dbe4aa6
DebankitaDutta <Debankitadutta82252@gmail.com>
2023-03-08 20:01:45 +0530

    first commit

---
+++ node_dockerization/index.js
@@ -0,0 +1,14 @@
+const express= require('express')
+const app= express()
+const port= 3000
+
+app.get("/",(req,res)=>{
+    res.send("wtf Deb??")
+})
+app.get("/ping",(req,res)=>{
+    res.send("hello pong-deb")
+})
+
+app.listen(port,()=>{
+    console.log(`APP listens at port ${port}`)
+})
